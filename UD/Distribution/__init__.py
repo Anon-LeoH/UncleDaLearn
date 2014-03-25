a5fef443b9e6085656af4dfc4377751c75437aeb
Anon-LeoH <lyu159494@gmail.com>
2014-03-25 19:57:27 +0800

    25-3-2014 v0

--- UD/Distribution/__init__.py
+++ UD/Distribution/__init__.py
@@ -4,105 +4,135 @@ from Area import *
 import random
 
 class distributionObject(mathObject):
-	def __init__(self):
-		self.type = "distribution"
-		self.e = 0
-		self.s = 0
+    def __init__(self):
+        self.type = "distribution"
+        self.e = 0
+        self.s = 0
 
-	def cal(self, val):
-		pass
+    def cal(self, val):
+        pass
 
-	def val(self):
-		pass
+    def val(self):
+        pass
 
 class PointDistribution(distributionObject):
-	def __init__(self, point):
-		self.value = point
-		self.e = self.value
-		self.s = 0
-	
-	def cal(self, val):
-		if val == self.value:
-			return 1
-		else:
-			return 0
+    def __init__(self, point):
+        self.value = point
+        self.e = self.value
+        self.s = 0
+    
+    def cal(self, val):
+        if val == self.value:
+            return 1
+        else:
+            return 0
 
-	def val(self):
-		return self.value
+    def val(self):
+        return self.value
 
 class BernoulliDistribution(distributionObject):
-	def __init__(self, p):
-		self.p = p
-		self.e = p
-		self.s = 1 - p
-	
-	def cal(self, val):
-		if val == 1:
-			return p
-		elif val == 0:
-			return 1 - p
-		else:
-			return None
-	
-	def val(self):
-		p = random.random()
-		if p > self.p:
-			return 0
-		else:
-			return 1
+    def __init__(self, p):
+        self.p = p
+        self.e = p
+        self.s = 1 - p
+    
+    def cal(self, val):
+        if val == 1:
+            return p
+        elif val == 0:
+            return 1 - p
+        else:
+            return None
+    
+    def val(self):
+        p = random.random()
+        if p > self.p:
+            return 0
+        else:
+            return 1
 
 class BinomialDistribution(distributionObject):
-	def __init__(self, p, n):
-		self.p = p
-		self.n = n
-		self.e = n * p
-		self.s = n * p * (1 - p)
-		self.func = binomialDisFunction(n, p)
-	
-	def cal(self, k):
-		return self.func.cal(k)
+    def __init__(self, p, n):
+        self.p = p
+        self.n = n
+        self.e = n * p
+        self.s = n * p * (1 - p)
+        self.innerFunc = binomialDisFunction(n, p)
+    
+    def cal(self, k):
+        return self.func.cal(k)
 
-	def val(self):
-		rlt = 0
-		for i in xrange(self.n):
-			tmp = random.random()
-			if tmp <= p:
-				rlt += 1
-		return rlt
+    def val(self):
+        rlt = 0
+        for i in xrange(self.n):
+            tmp = random.random()
+            if tmp <= p:
+                rlt += 1
+        return rlt
 
 class GeometricDistribution(distributionObject):
-	def __init__(self, p):
-		self.p = p
-		self.e = 1 / p
-		self.s = (1 - p) / p / p
-	
-	def cal(self, k):
-		return (1 - p) ^^ (k - 1) * p
+    def __init__(self, p):
+        self.p = p
+        self.e = 1 / p
+        self.s = (1 - p) / p / p
+    
+    def cal(self, k):
+        return (1 - p) ** (k - 1) * p
 
-	def val(self):
-		rlt = 0
-		while random.random() > self.p:
-			rlt += 1
-		return rlt
+    def val(self):
+        rlt = 0
+        while random.random() > self.p:
+            rlt += 1
+        return rlt
 
 class PossionDistribution(distributionObject):
-	def __init__(self, fy):
-		self.fy = fy
-		self.e = fy
-		self.s = fy
-	
-	def cal(self, k):
-		return (self.fy ^^ k) * (math.exp( (-1) * self.fy )) / math.factorial(k)
+    def __init__(self, fy):
+        self.fy = fy
+        self.e = fy
+        self.s = fy
+    
+    def cal(self, k):
+        return (self.fy ** k) * (math.exp( (-1) * self.fy )) / math.factorial(k)
 
-	def val(self):
-		rlt = 0
-		tmp = self.fy
-		while tmp > 0:
-			pos = random.random()
-			if pos <= tmp:
-				tmp -= pos
-				rlt += 1
-			else:
-				tmp = 0
-		return rlt
+    def val(self):
+        rlt = 0
+        tmp = self.fy
+        while tmp > 0:
+            pos = random.random()
+            if pos <= tmp:
+                tmp -= pos
+                rlt += 1
+            else:
+                tmp = 0
+        return rlt
+
+class UniformDistribution(distributionObject):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.e = (a + b) / 2
+        self.s = ((b - a) ** 2) / 12
+
+    def cal(self, tp):
+        return (tp[1] - tp[0]) / (b - a)
+
+    def val(self):
+        return math.uniform(a, b)
+
+class NormalDistribution(distributionObject):
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+        self.e = mu
+        self.s = sigma ** 2
+        self.innerFunc = GaussianFunction(mu, sigma)
+
+    def cal(self, x):
+        return self.innerFunc.cal(x)
+
+    def val(self):
+        delta = math.sqrt( (-1) * 2 * (sigma ** 2) * ( 0.5 * math.log(2 * np.pi
+        * (sigma ** 2)) + math.log(random.random()) ) )
+        return delta + mu
+
 
--- UD/Function/__init__.py
+++ UD/Function/__init__.py
@@ -5,78 +5,78 @@ import numpy as np
 import random
 
 class functionObject(mathObject):
-	def __init__(self):
-		self.type = "function"
-		self.theta = []
-		self.initArea = None
-	
-	def cal(self, x):
-		pass
+    def __init__(self):
+        self.type = "function"
+        self.theta = []
+        self.initArea = None
+    
+    def cal(self, x):
+        pass
 
 
 class constantFunction(functionObject):
-	def __init__(self, initArea, value):
-		self.initArea = initArea
-		self.valueArea = value
-	
-	def cal(self, x):
-		if self.initArea.include(x):
-			return self.valueArea
-		else:
-			return None
-	
+    def __init__(self, initArea, value):
+        self.initArea = initArea
+        self.valueArea = value
+    
+    def cal(self, x):
+        if self.initArea.include(x):
+            return self.valueArea
+        else:
+            return None
+    
 class linearFunction(functionObject):
-	def __init__(self, initArea, theta):
-		self.theta = theta
-		self.initArea = initArea
-	
-	def cal(self, x):
-		if not isinstance(x, list):
-			return None
-		if not len(self.theta) == len(x):
-			return None
-		return sum( [x[i] * theta[i] for i in xrange(len(x))] )
+    def __init__(self, initArea, theta):
+        self.theta = theta
+        self.initArea = initArea
+    
+    def cal(self, x):
+        if not isinstance(x, list):
+            return None
+        if not len(self.theta) == len(x):
+            return None
+        return sum( [x[i] * theta[i] for i in xrange(len(x))] )
 
 class GaussianFunction(functionObject):
-	def __init__(self, initArea = Area("continous", [[NegIf, PosIf]]), mu = 0, sigma = 1):
-		self.initArea = initArea
-		self.mu = mu
-		self.sigma = sigma
-	
-	def cal(self, x):
-		if not isinstance(x, list):
-			val = 1.0 / (self.sigma * math.sqrt(2 * np.pi)) * math.exp( (-1) * ((x - self.mu) ** 2) / (2 * (self.sigma ** 2)) )
-			return val
-		else:
-			val = []
-			for item in x:
-				val.append(self.cal(item))
-			return val
+    def __init__(self, mu = 0, sigma = 1, initArea = Area("continous", [[NegIf, PosIf]])):
+        self.initArea = initArea
+        self.mu = mu
+        self.sigma = sigma
+    
+    def cal(self, x):
+        if not isinstance(x, list):
+            val = 1.0 / (self.sigma * math.sqrt(2 * np.pi)) * math.exp( (-1) * ((x - self.mu) ** 2) / (2 * (self.sigma ** 2)) )
+            return val
+        else:
+            val = []
+            for item in x:
+                val.append(self.cal(item))
+            return val
 
 class compoundFunction(functionObject):
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def cal(self, n, k):
-		if k < (n / 2):
-			k = n - k
-		sum1 = 0
-		sum2 = 0
-		for i in xrange(k+1, n+1):
-			sum1 += math.log(i)
-		for i in xrange(1, n-k+1):
-			sum2 += math.log(i)
-		tmp = sum1 - sum2
-		return math.exp(tmp)
+    def cal(self, n, k):
+        if k < (n / 2):
+            k = n - k
+        sum1 = 0
+        sum2 = 0
+        for i in xrange(k+1, n+1):
+            sum1 += math.log(i)
+        for i in xrange(1, n-k+1):
+            sum2 += math.log(i)
+        tmp = sum1 - sum2
+        return math.exp(tmp)
 
 class binomialDisFunction(functionObject):
-	def __init__(self, n, p):
-		self.n = n
-		self.p = p
-		self.partFunc = compoundFunction()
-	
-	def cal(self, k):
-		tmpval = self.partFunc.cal(self.n, k)
-		return (p ^^ k) * ((1-p) ^^ (self.n - k)) * tmpval
+    def __init__(self, n, p):
+        self.n = n
+        self.p = p
+        self.partFunc = compoundFunction()
+    
+    def cal(self, k):
+        tmpval = self.partFunc.cal(self.n, k)
+        return (p ** k) * ((1-p) ** (self.n - k)) * tmpval
 
 
